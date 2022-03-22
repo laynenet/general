22de0ac7a33f90393c782d3bd74f3d44a021833a
DonovanA03 <dknowallen@gmail.com>
2022-03-22 12:34:24 -0500

    update thang

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import 'virtual:windi.css'
+
+	let name = 'world';
+</script>
+
+  <div class="flex">
+	<div class="basis 1/4 bg-dark-400 w-60 h-300"></div>
+  </div>
+
+  <div class="flex flex-wrap">
+	<div class="basis 1/2 bg-red-800 w-500 h-15"></div>
+	<div class="basis 1/2 bg-red-800 w-500 h-15"></div>
+  </div>
+  <style>
+	  :global(body){
+		  padding:0;
+	  }
+  </style>
+
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,3 @@
-<!-- Anything in here is transfered to all pages -->
-<div class="bg-blue-400">This div here is located in __layout.svelte<br>Anything you put in __layout.svelte will be added to all pages<br>Really useful for a persistant navigation bar or soemthing</div>
 
 <!-- <slot> is essentail where the page being visited is fitted in so don't delete it -->
 <slot></slot>
